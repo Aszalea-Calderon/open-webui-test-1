2a6048f19fc4809431f4c3ee8438e3359d9da2c7
Aszalea-Calderon <c.aszalea@gmail.com>
2025-05-24 11:25:53 -0400

    Got it running

---
+++ !!README_ASZ.py
@@ -0,0 +1,7 @@
+##To get started with the code, start the front end first by running the following command in the terminal:
+#  npm run dev (# This will start the front end server and open the application in your default web browser.)
+
+#To run the backend server, get into the VIR by doing the following command:
+# cd backend
+# conda activate open-webui (to get into the environment)
+# start_windows.bat
